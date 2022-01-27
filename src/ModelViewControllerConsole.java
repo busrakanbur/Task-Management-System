1beb51bb594c87818bf745756a37c55eefb33ee5
busrakanbur <nolensvolens99@gmail.com>
2022-01-27 16:27:52 +0300

    new2

--- src/ModelViewControllerConsole.java
+++ src/ModelViewControllerConsole.java
@@ -1,69 +1,61 @@
 
 import java.util.*;
 
-
 public class ModelViewControllerConsole {
 
-	public static void main(String[] args) throws Exception {
-		// Connect to database
-		connectToDatabase();
-                		
-		// Model View Controller (MVC)
-		// Router knows all the controllers
-		Map<String, Controller> router = new HashMap<>();		
-		router.put("MainMenu", new Controller(new MainMenuView(), new NopModel()));
-		router.put("UserAccount", new Controller(new UserAccountView(), new UserAccountModel()));
-                
+    public static void main(String[] args) throws Exception {
+        // Connect to database
+        connectToDatabase();
+
+        // Model View Controller (MVC)
+        // Router knows all the controllers
+        Map<String, Controller> router = new HashMap<>(); //*** sonra bak		
+        router.put("MainMenu", new Controller(new MainMenuView(), new NopModel()));
+        router.put("UserAccount", new Controller(new UserAccountView(), new UserAccountModel()));
+
 //main menu çağırıldıktan sonra, mainmenuview kısmında useraccount çağırılıyor!!!!!!!
 //return new ViewData("UserAccount", operationName, new HashMap<>()); şeklinde
+        router.put("ManagerLoginMenu", new Controller(new ManagerLoginMenuView(), new NopModel()));
+        router.put("Manager", new Controller(new ManagerView(), new ManagerModel()));
 
-                router.put("ManagerLoginMenu", new Controller(new ManagerLoginMenuView(), new NopModel()));
-		router.put("Manager", new Controller(new ManagerView(), new ManagerModel()));
+        ViewData viewData = new ViewData("MainMenu", "");
+        ViewData viewData2 = new ViewData("ManagerLoginMenu", "");
 
-		ViewData viewData = new ViewData("MainMenu", "");
-                ViewData viewData2 = new ViewData("ManagerLoginMenu", ""); 
-                /*
-                if(viewData.functionName == "signin"){
-                    do {
-                            // Model, View, and Controller
-                            Controller controller = router.get(viewData2.functionName);
-                            ModelData modelData = controller.executeModel(viewData2);
-                            viewData2 = controller.getView(modelData, viewData2.functionName, viewData2.operationName);
+        do {
+            // Model, View, and Controller
+            Controller controller = router.get(viewData.functionName);
+            ModelData modelData = controller.executeModel(viewData);
+            viewData = controller.getView(modelData, viewData.functionName, viewData.operationName);
 
-                            System.out.println();
-                            System.out.println("-------------------------------------------------");
-                            System.out.println();
-                    }
-                    while (viewData2.functionName != null);
-                    }
-                else{
+            System.out.println();
+            System.out.println("-------------------------------------------------");
+            System.out.println();
+
+            if (viewData.functionName == "signup.gui") {
                 do {
-                            // Model, View, and Controller
-                            Controller controller = router.get(viewData.functionName);
-                            ModelData modelData = controller.executeModel(viewData);
-                            viewData = controller.getView(modelData, viewData.functionName, viewData.operationName);
+                    // Model, View, and Controller
+                    Controller controller2 = router.get(viewData2.functionName);
+                    ModelData modelData2 = controller2.executeModel(viewData2);
+                    viewData2 = controller2.getView(modelData2, viewData2.functionName, viewData2.operationName);
 
-                            System.out.println();
-                            System.out.println("-------------------------------------------------");
-                            System.out.println();
-                    }
-                    while (viewData.functionName != "signin");
-                
-                do {
-                            // Model, View, and Controller
-                            Controller controller = router.get(viewData2.functionName);
-                            ModelData modelData = controller.executeModel(viewData2);
-                            viewData2 = controller.getView(modelData, viewData2.functionName, viewData2.operationName);
+                    System.out.println();
+                    System.out.println("-------------------------------------------------");
+                    System.out.println();
+                } while (viewData2.functionName != null); 
+            }
+            
+            
+            
+            
+            
+            else if(viewData.functionName == "select"){
+                System.out.println("selecet çalıştı");
+            }
 
-                            System.out.println();
-                            System.out.println("-------------------------------------------------");
-                            System.out.println();
-                    }
-                    while (viewData2.functionName != null);
-                       
-                }*/
-                
-                if(viewData.functionName != "signin"){ //bu kısım çalışmıyor bile, kimse signin e bakmıyor
+        } while (viewData.functionName != null); //8 quit
+
+        /*
+                if(viewData.functionName != "signin.gui"){ //bu kısım çalışmıyor bile, kimse signin e bakmıyor
                     //şu an signupta menu view değişiyor sadece
                     
                     do {
@@ -76,7 +68,7 @@ public class ModelViewControllerConsole {
                             System.out.println("-------------------------------------------------");
                             System.out.println();
                     }
-                    while (viewData.functionName != "signin"); //bu kısım hiçbir işe yaramıyor herhalde
+                    while (viewData.functionName != "signin.gui"); //bu kısım hiçbir işe yaramıyor herhalde
                     //yarıyormuş, bunu null yapınca, main menu açılıyor quit diyorum, manager menuye gidiyor orda da quit demem gerekiyor.
                     //bir de manager menuye gidersem 7 diyip, ordan ilk denemede quitle çıkamıyorum, bir de delete her şeyi siliyor onu anlamadım
                     
@@ -107,40 +99,34 @@ public class ModelViewControllerConsole {
                     }
                     while (viewData2.functionName != null);
                     }
-                
-                
-		System.out.println();
-		System.out.println();
-		System.out.println("Program is ended...");
+         */
+        System.out.println();
+        System.out.println();
+        System.out.println("Program is ended...");
 
+        // Disconnect from database
+        disconnectFromDatabase();
+    }
 
-		// Disconnect from database
-		disconnectFromDatabase();
-	}
+    public static void connectToDatabase() {
+        DatabaseUtilities.host = "localhost:49669";
+        DatabaseUtilities.databaseName = "Project2";
 
-	
-	public static void connectToDatabase() {
-		DatabaseUtilities.host = "localhost:49669";
-		DatabaseUtilities.databaseName = "Project2";
-		
-		try {
-			DatabaseUtilities.getConnection();
-		}
-		catch(Exception e) {
-			System.out.println("Exception occured : " + e);
-			return;
-		}		
-	}
-	
-	public static void disconnectFromDatabase() {
-		try {
-			DatabaseUtilities.disconnect();
-		}
-		catch(Exception e) {
-			System.out.println("Error disconnecting from database : " + e);
-			return;
-		}		
-	}
+        try {
+            DatabaseUtilities.getConnection();
+        } catch (Exception e) {
+            System.out.println("Exception occured : " + e);
+            return;
+        }
+    }
 
-	
+    public static void disconnectFromDatabase() {
+        try {
+            DatabaseUtilities.disconnect();
+        } catch (Exception e) {
+            System.out.println("Error disconnecting from database : " + e);
+            return;
+        }
+    }
+
 }
